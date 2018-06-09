ac9527ef00c3843d617faa6c38e44598cd4bc4c7
Menencia <menencia@gmail.com>
2018-06-09 21:57:23 +0200

    add mongoose

--- src/bot.ts
+++ src/bot.ts
@@ -1,10 +1,20 @@
 import * as discord from 'discord.js'
+import { MongoError } from 'mongodb'
+import * as mongoose from 'mongoose'
 
 export class Bot {
 
-  constructor(public client: discord.Client) {}
+  constructor(
+    public client: discord.Client
+  ) {}
 
   public start() {
+
+    const url = process.env.MONGODB_URI || ''
+
+    mongoose.connect(url).then((db) => {
+      console.log(db)
+    }, (err: MongoError) => console.log(err))
 
     this.client.on('ready', () => {
       console.log('started...')
