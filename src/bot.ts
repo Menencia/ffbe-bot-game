8d74520737c8276c8a9a2e365d4f67e306091175
Menencia <menencia@gmail.com>
2018-06-09 19:56:32 +0200

    fix spelling

--- src/bot.ts
+++ src/bot.ts
@@ -13,7 +13,7 @@ export class Bot {
     this.client.on('message', (message) => {
       if (message.content === 'ping') {
         message.author.send('pong')
-        message.channel.send('poong')
+        message.channel.send('pong')
       }
     })
 
