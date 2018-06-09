1533c6ff70a49958e979b48c4edd714fc88c4a3a
Menencia <menencia@gmail.com>
2018-06-09 22:28:48 +0200

    first implementation of mangoose

--- src/bot.ts
+++ src/bot.ts
@@ -6,23 +6,45 @@ export class Bot {
 
   constructor(
     public client: discord.Client
-  ) {}
+  ) {
+  }
 
   public start() {
 
     const url = process.env.MONGODB_URI || ''
 
-    mongoose.connect(url).then((db) => {
-      console.log(db)
+    mongoose.connect(url).then(() => {
+
+      const Schema = mongoose.Schema
+      const userSchema = new Schema({
+        name: String,
+        id: String,
+        created: Date
+      })
+
+      mongoose.model('User', userSchema)
+
+      this.setupDiscord()
     }, (err: MongoError) => console.log(err))
 
+  }
+
+  public setupDiscord() {
     this.client.on('ready', () => {
       console.log('started...')
     })
 
     this.client.on('message', (message) => {
-      if (message.content === '!pull') {
+      if (message.content === '/play') {
         if (message.channel instanceof discord.DMChannel) {
+          console.log(message.author.id)
+          mongoose
+            .model('User')
+            .where('id', message.author.id)
+            .findOne((err, user) => {
+              console.log(err)
+              console.log(user)
+            })
           message.author.send('Not available yet!')
         }
       }
