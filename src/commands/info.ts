ebda0a9e18cce29425b687d4929da7fd3f2e15f4
Menencia <menencia@gmail.com>
2018-06-10 03:25:02 +0200

    fix info command

--- src/commands/info.ts
+++ src/commands/info.ts
@@ -1,10 +1,13 @@
-import { Message } from 'discord.js'
+import { Message, User } from 'discord.js'
 import * as mongoose from 'mongoose'
 
 export class InfoAction {
 
+  /**
+   * Handle command
+   * @param message
+   */
   public onMessage(message: Message) {
-
     const userModel = mongoose.model('User')
     userModel
       .where('id', message.author.id)
@@ -12,10 +15,22 @@ export class InfoAction {
         if (err) {
           console.log(err)
         } else if (user) {
-          message.author.send(user)
+          const msg = this.buildMsg(user)
+          message.author.send(msg)
         }
       })
+  }
 
+  /**
+   * Build user infos from datastore
+   * @param user
+   */
+  private buildMsg(user: User) {
+    const html = `\`\`\`
+      Username: ${user.username}
+      \`\`\`
+    `
+    return html
   }
 
 }
