90dc443a7cdaf92ce1b0457105e0e0f51d892a4c
Menencia <menencia@gmail.com>
2018-06-10 04:36:47 +0200

    summon rank preview

--- src/commands/pull.ts
+++ src/commands/pull.ts
@@ -6,9 +6,27 @@ export class PullAction {
 
   public async onMessage(message: Message) {
 
+    // @todo check lapis
+
     const rawUnits: any = await this.fetchUnits()
     const units = this.getUnits(rawUnits)
     message.author.send(`${units.length} units in the pool`)
+
+    let rank
+    const rng = Math.random()
+    if (rng < 3) {
+      rank = 5
+    } else if (rng < 22) {
+      rank = 4
+    } else {
+      rank = 3
+    }
+
+    message.author.send(`You pulled a ${rank}* unit`)
+
+    // @todo add the unit to user and remove 250 lapis
+
+    // @todo show to public channel if rank=5
 
     /*const userModel = mongoose.model('User')
     userModel
@@ -33,10 +51,9 @@ export class PullAction {
     const units = []
     for (const id in rawUnits) {
       if (rawUnits.hasOwnProperty(id)) {
-        units.push(rawUnits[id])
+        units.push(rawUnits[id]) // @todo sort in 3 tables : 3* / 4* / 5*
       }
     }
-    console.log(units)
     return units
   }
 
